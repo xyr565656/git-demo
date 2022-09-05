34d21b74ba7d5104093f9b434d864afcdccfda6f
hy <hy@qq.com>
2022-09-05 14:03:45 +0800

    zzzx

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git1!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("master test!");
+        System.out.println("hot-fix test!");
+        System.out.println("push test!");
+    }
+}
